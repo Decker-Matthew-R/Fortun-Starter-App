4841a89d7392f8b4b807a8103c25ce27d10ae1ef
Matt Decker <matthew.r.decker4.mil@army.mil>
2024-03-16 19:29:00 -0500

    Updates to testAll

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,17 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import {render, screen, waitFor} from '@testing-library/react';
+import {describe, expect, it, vi} from 'vitest';
 import App from "../App";
+import {act} from "react-dom/test-utils";
 
 describe('render app', () => {
-  const renderApp = () => render(<App />)
+  const renderApp = () => render(<App/>)
 
-  it('should render app images', () => {
+  it('should render app images', async () => {
     renderApp();
 
     const countButton = screen.getByRole('button', {name: 'count is 0'});
 
     expect(countButton).toBeVisible();
 
+    act(() => {
+      waitFor(() => {
+        countButton.click();
+        expect("count is 1").toBeVisible();
+      });
+    })
   });
-
 });
