eded18fc8587d0de04e83387cf6ba57537091ca5
Matt Decker <matthew.r.decker4.mil@army.mil>
2024-03-17 12:55:42 -0500

    Initial Stages of MUI Theme Complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import './App.css';
+import {Button, Typography} from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,18 +29,21 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Typography variant={'h2'}>Vite + React</Typography>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button variant={"contained"} onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
+        <Button color={'secondary'} variant={"contained"}>
+          This Button Does Nothing
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
+      <Typography variant={'body1'} className='read-the-docs'>
         Click on the Vite and React logos to learn more
-      </p>
+      </Typography>
     </>
   );
 }
--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,15 +1,15 @@
-import {render, screen, waitFor} from '@testing-library/react';
-import {describe, expect, it} from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import App from '../App';
-import {act} from 'react-dom/test-utils';
+import { act } from 'react-dom/test-utils';
 
 describe('render app', () => {
-  const renderApp = () => render(<App/>);
+  const renderApp = () => render(<App />);
 
   it('should render app images', () => {
     renderApp();
 
-    const countButton = screen.getByRole('button', {name: 'count is 0'});
+    const countButton = screen.getByRole('button', { name: 'count is 0' });
 
     expect(countButton).toBeVisible();
 
