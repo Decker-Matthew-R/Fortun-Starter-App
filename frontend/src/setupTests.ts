bb99d4752aae3d983906371f9c0db464028032fe
Matt Decker <matthew.r.decker4.mil@army.mil>
2026-02-03 11:21:25 -0600

    Updated frontend deps

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,13 +1,10 @@
-import { afterAll, afterEach, beforeAll, expect } from 'vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 import { setupServer } from 'msw/node';
 import '@testing-library/jest-dom';
-import matchers from '@testing-library/jest-dom/matchers';
 import { config } from 'react-transition-group';
 
 // Removes MUI animations for testing
 config.disabled = true;
-
-expect.extend(matchers);
 
 export const server = setupServer();
 beforeAll(() => {
