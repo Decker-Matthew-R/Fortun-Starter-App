de4def7b69c382fbc5b1f1832864aed49e040a6c
Matt Decker <matthew.r.decker4.mil@army.mil>
2026-02-05 11:42:14 -0600

    Updated .gitignore

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -5,22 +5,22 @@ import App from '../App.tsx';
 import * as MetricsClient from '../metrics/client/MetricsClient';
 
 describe('render app', () => {
-  it('should render and increment count', async () => {
-    // Mock the metrics service
-    vi.spyOn(MetricsClient, 'useMetrics').mockReturnValue({
-      saveMetricEvent: vi.fn().mockResolvedValue(undefined),
+    it('should render and increment count', async () => {
+        // Mock the metrics service
+        vi.spyOn(MetricsClient, 'useMetrics').mockReturnValue({
+            saveMetricEvent: vi.fn().mockResolvedValue(undefined),
+        });
+
+        const user = userEvent.setup();
+        render(<App />);
+
+        const countButton = screen.getByRole('button', { name: 'count is 0' });
+        expect(countButton).toBeVisible();
+
+        await user.click(countButton);
+
+        await waitFor(() => {
+            expect(screen.getByRole('button', { name: 'count is 1' })).toBeVisible();
+        });
     });
-
-    const user = userEvent.setup();
-    render(<App />);
-
-    const countButton = screen.getByRole('button', { name: 'count is 0' });
-    expect(countButton).toBeVisible();
-
-    await user.click(countButton);
-
-    await waitFor(() => {
-      expect(screen.getByRole('button', { name: 'count is 1' })).toBeVisible();
-    });
-  });
 });
