7da13da35ce261db9c2f70000d091cc50f73585a
Matt Decker <matthew.r.decker4.mil@army.mil>
2026-02-05 12:28:40 -0600

    Added Test Coverage

--- src/test/java/com/fortuna/FortunaApplicationTests.java
+++ src/test/java/com/fortuna/FortunaApplicationTests.java
@@ -2,10 +2,21 @@ package com.fortuna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class FortunaApplicationTests {
+@TestPropertySource(
+    properties = {
+        "spring.security.oauth2.client.registration.google.client-id=test-client-id",
+        "spring.security.oauth2.client.registration.google.client-secret=test-client-secret"
+    })
+class AtlasApplicationTests {
 
     @Test
     void contextLoads() {}
+
+    @Test
+    void mainMethodTest() {
+        FortunaApplication.main(new String[] {});
+    }
 }
